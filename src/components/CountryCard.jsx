6b78a3029b8a815e4d10b8677b8515fca3810ae8
Arnav Sakhare <arnav.sakhare123@gmail.com>
2024-06-21 03:41:50 +0530

    Fixed a bug

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,21 +1,11 @@
 import React from 'react'
+import './CountryCard.css'
 
 const CountryCard = ({country}) => {
 
   return (
-    <div style={{
-        height: "200px",
-        width: "200px",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        border: "1px solid black",
-        borderRadius: "8px",
-        margin: "20px",
-        textAlign: "center"
-    }}>
-        <img style={{height: "100px", width: "150px"}} src={country.flags.png} alt={country.flags.alt} />
+    <div className='countryCard'>
+        <img src={country.flags.png} alt={country.flags.alt} />
         <h3>{country.name.common}</h3>
     </div>
   )
